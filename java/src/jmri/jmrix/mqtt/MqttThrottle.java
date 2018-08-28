10707adf4b03253677fd44e71b3887c6dadd5ece
Paul Melnikov <positron96@gmail.com>
2018-08-28 17:45:48 +0700

    Throttle direction, speed, functions work.

--- java/src/jmri/jmrix/mqtt/MqttThrottle.java
+++ java/src/jmri/jmrix/mqtt/MqttThrottle.java
@@ -1,6 +1,6 @@
 package jmri.jmrix.mqtt;
 
-import java.util.concurrent.LinkedBlockingQueue;
+import java.lang.reflect.Field;
 import jmri.DccLocoAddress;
 import jmri.DccThrottle;
 import jmri.LocoAddress;
@@ -22,13 +22,6 @@ public class MqttThrottle extends AbstractThrottle implements MqttEventListener 
 
     protected MqttAdapter mqttAdapter = null;
 
-    // status of the throttle
-    protected static final int THROTTLEIDLE = 0;  // Idle Throttle
-    protected static final int THROTTLESPEEDSENT = 2;  // Sent speed/dir command to locomotive
-    protected static final int THROTTLEFUNCSENT = 4;   // Sent a function command to locomotive.
-
-    public int requestState = THROTTLEIDLE;
-
     protected int address;
     
     private String sendTopicBase;
@@ -38,19 +31,13 @@ public class MqttThrottle extends AbstractThrottle implements MqttEventListener 
         super(memo);
         mqttAdapter = controller;
         
-        if (log.isDebugEnabled()) {
-            log.debug("DCCppThrottle constructor");
-        }
+        log.debug("MqttThrottle constructor");
     }
 
-    /**
-     * Constructor
-     */
     public MqttThrottle(SystemConnectionMemo memo, LocoAddress address, MqttAdapter controller) {
         super(memo);
         this.mqttAdapter = controller;
-        this.setDccAddress(address.getNumber() 
-        );
+        this.setDccAddress(address.getNumber() );
         this.speedIncrement = SPEED_STEP_128_INCREMENT;
         this.speedStepMode = DccThrottle.SpeedStepMode128;
 
@@ -59,145 +46,138 @@ public class MqttThrottle extends AbstractThrottle implements MqttEventListener 
                 = f10Momentary = f11Momentary = f12Momentary = false;
 
         sendTopicBase = "loco/"+address.getNumber()+"/";
-        if (log.isDebugEnabled()) {
-            log.debug("DCCppThrottle constructor called for address " + address);
+        log.debug("DCCppThrottle constructor called for address {}", address);
+
+    }
+    
+    private void setF(int f, boolean v)  {
+        log.debug("set F{} to {}", f, v);
+        try {
+            Field field = getClass().getSuperclass().getDeclaredField("f"+f);
+            boolean old = field.getBoolean(this);
+            field.setBoolean(this, v);
+            sendMessage("F"+f, v ? "1" : "0");
+            if (old != v) notifyPropertyChangeListener("F"+f, old, v);
+        } catch (NoSuchFieldException | SecurityException | 
+                IllegalArgumentException | IllegalAccessException ex) {
+            log.error("could not set Function", ex);
         }
     }
     
-    private byte b2b(boolean v) {
-        return v==false ? (byte)0 : (byte)1;
-    }
-
-    /**
-     * Send the DCC++ message to set the state of locomotive direction and
-     * functions F0, F1, F2, F3, F4
-     */
+    
     @Override
-    protected void sendFunctionGroup1() {
-        log.debug("sendFunctionGroup1(): f0 {} f1 {} f2 {} f3 {} f4{}",
-                f0, f1, f2, f3, f4);
+    public void setF0(boolean v) {  setF(0, v);  }
 
-        sendMessage("fg1", new byte[]{ b2b(f0), b2b(f1), b2b(f2), b2b(f3), b2b(f4) } );
-    }
-
-    /**
-     * Send the DCC++ message to set the state of functions F5, F6, F7, F8
-     */
     @Override
-    protected void sendFunctionGroup2() {
-        
-        log.debug("sendFunctionGroup2(): f5 {} f6 {} f7 {} f8",
-                f5, f6, f7, f8);
+    public void setF1(boolean v) {  setF(1, v);  }
 
-        sendMessage("fg2", new byte[]{ b2b(f5), b2b(f6), b2b(f7), b2b(f8) });
-    }
-
-    /**
-     * Send the DCC++ message to set the state of functions F9, F10, F11, F12
-     */
     @Override
-    protected void sendFunctionGroup3() {
-        
-        log.debug("sendFunctionGroup3(): f9 {} f10 {} f11 {} f12",
-                f9, f10, f11, f12);
+    public void setF2(boolean v) {  setF(2, v);  }
 
-        sendMessage("fg3", new byte[]{ b2b(f9), b2b(f10), b2b(f11), b2b(f12) } );
-    }
-
-    /**
-     * Send the DCC++ message to set the state of functions F13, F14, F15, F16,
-     * F17, F18, F19, F20
-     */
     @Override
-    protected void sendFunctionGroup4() {
-        
-         log.debug("sendFunctionGroup4(): f13 {} f14 {} f15 {} f16",
-                f13, f14, f15, f6);
-
-        sendMessage("fg4", new byte[] { b2b(f13), b2b(f14), b2b(f15), b2b(f16), 
-            b2b(f17), b2b(f18), b2b(f19), b2b(f20) });
-    }
-
-    /**
-     * Send the DCC++ message to set the state of functions F21, F22, F23, F24,
-     * F25, F26, F27, F28
-     */
+    public void setF3(boolean v) {  setF(3, v);  }
     @Override
-    protected void sendFunctionGroup5() {
-        log.debug("sendFunctionGroup5(): f21 {} f22 {} f23 {} f24 {} f25 {} f26 {} f27 {} f28 {}",
-                f21, f22, f23, f24, f25, f26, f27, f28);
+    public void setF4(boolean v) {  setF(4, v);  }
 
-        sendMessage("fg5", new byte[]{ b2b(f21), b2b(f22), b2b(f23), b2b(f24), 
-            b2b(f25), b2b(f26), b2b(f27), b2b(f28) } );
-    }
-
-    /**
-     * Send the DCC++ message to set the Momentary state of locomotive functions
-     * F0, F1, F2, F3, F4
-     */
     @Override
-    protected void sendMomentaryFunctionGroup1() {
-        log.debug("sendMomentaryFunctionGroup1(): f0 {} f1 {} f2 {} f3 {} f4{}",
-                f0Momentary, f1Momentary, f2Momentary, f3Momentary, f4Momentary);
+    public void setF5(boolean v) {  setF(5, v);  }
 
-        sendMessage("fg1m", new byte[]{ b2b(f0Momentary), b2b(f1Momentary), 
-            b2b(f2Momentary), b2b(f3Momentary), b2b(f4Momentary) } );
-    }
-
-    /**
-     * Send the DCC++ message to set the momentary state of functions F5, F6,
-     * F7, F8
-     */
     @Override
-    protected void sendMomentaryFunctionGroup2() {
-        log.debug("sendMomentaryFunctionGroup2(): f5 {} f6 {} f7 {} f8",
-                f5Momentary, f6Momentary, f7Momentary, f8Momentary);
+    public void setF6(boolean v) {  setF(6, v);  }
 
-        sendMessage("fg2m", new byte[]{ b2b(f5Momentary), b2b(f6Momentary), 
-            b2b(f7Momentary), b2b(f8Momentary) });
-    }
-
-    /**
-     * Send the DCC++ message to set the momentary state of functions F9, F10,
-     * F11, F12
-     */
     @Override
-    protected void sendMomentaryFunctionGroup3() {
-        log.debug("sendMomentaryFunctionGroup3(): f9 {} f10 {} f11 {} f12",
-                f9Momentary, f10Momentary, f11Momentary, f12Momentary);
+    public void setF7(boolean v) {  setF(7, v);  }
 
-        sendMessage("fg3m", new byte[]{ b2b(f9Momentary), b2b(f10Momentary), 
-            b2b(f11Momentary), b2b(f12Momentary) } );
-    }
-
-    /**
-     * Send the DCC++ message to set the momentary state of functions F13, F14,
-     * F15, F16 F17 F18 F19 F20
-     */
     @Override
-    protected void sendMomentaryFunctionGroup4() {
-         log.debug("sendMomentaryFunctionGroup4(): f13 {} f14 {} f15 {} f16",
-                f13Momentary, f14Momentary, f15Momentary, f6Momentary);
+    public void setF8(boolean v) {  setF(8, v);  }
 
-        sendMessage("fg4m", new byte[] { b2b(f13Momentary), b2b(f14Momentary), 
-            b2b(f15Momentary), b2b(f16Momentary), b2b(f17Momentary), 
-            b2b(f18Momentary), b2b(f19Momentary), b2b(f20Momentary) });
-    }
-
-    /**
-     * Send the DCC++ message to set the momentary state of functions F21, F22,
-     * F23, F24 F25 F26 F27 F28
-     */
     @Override
-    protected void sendMomentaryFunctionGroup5() {
-        log.debug("sendMomentaryFunctionGroup5(): f21 {} f22 {} f23 {} f24 {} f25 {} f26 {} f27 {} f28 {}",
-                f21Momentary, f22Momentary, f23Momentary, f24Momentary, f25Momentary, f26Momentary, f27Momentary, f28Momentary);
+    public void setF9(boolean v) {  setF(9, v);  }
 
-        sendMessage("fg5m", new byte[]{ b2b(f21Momentary), b2b(f22Momentary), 
-            b2b(f23Momentary), b2b(f24Momentary), b2b(f25Momentary), 
-            b2b(f26Momentary), b2b(f27Momentary), b2b(f28Momentary) } );
+    @Override
+    public void setF10(boolean v) {  setF(10, v);  }
+
+    @Override
+    public void setF11(boolean v) {  setF(11, v);  }
+
+    @Override
+    public void setF12(boolean v) {  setF(12, v);  }
+
+    @Override
+    public void setF13(boolean v) {  setF(13, v);  }
+
+    @Override
+    public void setF14(boolean v) {  setF(14, v);  }
+
+    @Override
+    public void setF15(boolean v) {  setF(15, v);  }
+
+    @Override
+    public void setF16(boolean v) {  setF(16, v);  }
+
+    @Override
+    public void setF17(boolean v) {  setF(17, v);  }
+
+    @Override
+    public void setF18(boolean v) {  setF(18, v);  }
+
+    @Override
+    public void setF19(boolean v) {  setF(19, v);  }
+
+    @Override
+    public void setF20(boolean v) {  setF(20, v);  }
+
+    @Override
+    public void setF21(boolean v) {  setF(21, v);  }
+
+    @Override
+    public void setF22(boolean v) {  setF(22, v);  }
+
+    @Override
+    public void setF23(boolean v) {  setF(23, v);  }
+
+    @Override
+    public void setF24(boolean v){  setF(24, v);  }
+
+    @Override
+    public void setF25(boolean v) {  setF(25, v);  }
+    
+    @Override
+    public void setF26(boolean v) {  setF(26, v);  }
+
+    @Override
+    public void setF27(boolean v) {  setF(27, v);  }
+
+    @Override
+    public void setF28(boolean v) {  setF(28, v);  }
+    
+    
+    
+    @Override
+    protected void sendMomentaryFunctionGroup1() {   }
+
+    @Override
+    protected void sendMomentaryFunctionGroup2() {   }
+
+    @Override
+    protected void sendMomentaryFunctionGroup3() {   }
+
+    @Override
+    protected void sendMomentaryFunctionGroup4() {   }
+
+    @Override
+    protected void sendMomentaryFunctionGroup5() {   }
+    
+
+   
+    private void sendSpeed() {
+        int v = Math.round(getSpeedSetting() / getSpeedIncrement());
+        if (! super.getIsForward() ) {
+            v = -v;
+        }
+        sendMessage("speed", ""+v, false );
     }
-
+    
     /* 
      * setSpeedSetting - notify listeners and send the new speed to the
      * command station.
@@ -205,26 +185,19 @@ public class MqttThrottle extends AbstractThrottle implements MqttEventListener 
     @Override
     synchronized public void setSpeedSetting(float speed) {
         log.debug("set Speed to: {},  Current step mode is: {}",  speed, this.speedStepMode);
-        System.out.println("set Speed to: {},  Current step mode is: {}"+speed+", "+this.speedStepMode);
+        if (speed > 1) {
+            speed = (float) 1.0;
+        }
         super.setSpeedSetting(speed);
         if (speed < 0) {
-            /* we're sending an emergency stop to this locomotive only */
             sendEmergencyStop();
         } else {
-            if (speed > 1) {
-                speed = (float) 1.0;
-            }
-
-            sendMessage("speed", (""+Math.round(speed*127)).getBytes() );
+            sendSpeed();
         }
     }
 
-    /* Since xpressnet has a seperate Opcode for emergency stop,
-     * We're setting this up as a seperate protected function
-     */
-    protected void sendEmergencyStop() {
-        
-        sendMessage("EMGR_STOP", new byte[]{} );
+    private void sendEmergencyStop() {
+        sendMessage("EMGR_STOP", "" );
     }
 
     /* Since there is only one "throttle" command to the DCC++ base station,
@@ -233,22 +206,7 @@ public class MqttThrottle extends AbstractThrottle implements MqttEventListener 
     @Override
     public void setIsForward(boolean forward) {
         super.setIsForward(forward);
-        setSpeedSetting(this.speedSetting);
-    }
-
-    /*
-     * setSpeedStepMode - set the speed step value and the related
-     *                    speedIncrement value.
-     * <P>
-     * @param Mode - the current speed step mode - default should be 128
-     *              speed step mode in most cases
-     *
-     * NOTE: DCC++ only supports 128-step mode.  So we ignore the speed
-     * setting, even though we store it.
-     */
-    @Override
-    public void setSpeedStepMode(int Mode) {
-        super.setSpeedStepMode(Mode);
+        sendSpeed();
     }
 
     /**
@@ -264,7 +222,7 @@ public class MqttThrottle extends AbstractThrottle implements MqttEventListener 
         finishRecord();
     }
 
-    public int setDccAddress(int newaddress) {
+    private int setDccAddress(int newaddress) {
         address = newaddress;
         return address;
     }
@@ -286,38 +244,22 @@ public class MqttThrottle extends AbstractThrottle implements MqttEventListener 
         return new DccLocoAddress(address, address<128 ? LocoAddress.Protocol.DCC_SHORT : LocoAddress.Protocol.DCC_LONG);
     }
 
-    synchronized protected void sendMessage(String topic, byte[] msg) {
-        mqttAdapter.publish(sendTopicBase+topic, msg);
+    synchronized protected void sendMessage(String topic, String msg) {
+        mqttAdapter.publish(sendTopicBase+topic, msg.getBytes() );
+    }
+    
+    synchronized protected void sendMessage(String topic, String msg, boolean retain) {
+        mqttAdapter.publish(sendTopicBase+topic, msg.getBytes(), retain);
     }
 
     @Override
     public void notifyMqttMessage(String topic, String message) {
-        log.info("got "+message);
+        log.info("got {}: {}", topic, message);
     }
 
 
     // register for notification
     private final static Logger log = LoggerFactory.getLogger(MqttThrottle.class);
     
-    
-    /*
-    public static class MqttLocoAddress extends DccLocoAddress {
-        private final int addr;
 
-        public MqttLocoAddress(int addr) {
-            super(addr, )
-            this.addr = addr;
-        }
-
-        @Override
-        public int getNumber() {
-            return addr;
-        }
-
-        @Override
-        public Protocol getProtocol() {
-            return addr<128 ? Protocol.DCC_SHORT : Protocol.DCC_LONG;
-        }
-        
-    }*/
 }
